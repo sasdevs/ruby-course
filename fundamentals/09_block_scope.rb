23103ec47c5bb0c858fbb1f808a81c9dc63c3ddc
Dave <info@howmuchcomputer.com>
2016-10-04 08:58:14 -0400

    bscope

---
+++ fundamentals/09_block_scope.rb
@@ -0,0 +1,11 @@
+def block_local_parameter
+  x = 100
+  [1,2,3].each do |x|
+    puts "Parameter x is #{x}"
+    x = x + 10
+    puts "Reassigned to x in block; it's now #{x}"
+  end
+  puts "Outer x is still #{x}"
+end
+
+block_local_parameter
